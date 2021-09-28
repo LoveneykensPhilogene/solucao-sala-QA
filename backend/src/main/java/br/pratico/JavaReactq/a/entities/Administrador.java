d96c2855b60fa91d68120074f645389bf76ebe2b
Loveneykens Philogene <loveneykens@gmail.com>
2021-09-28 00:27:02 -0300

    tabela Administrador criada no Banco de dados

--- backend/src/main/java/br/pratico/JavaReactq/a/entities/Administrador.java
+++ backend/src/main/java/br/pratico/JavaReactq/a/entities/Administrador.java
@@ -2,11 +2,22 @@ package br.pratico.JavaReactq.a.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Administrador implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column
 	private String nome;
 	private String sobreNome;
 	private String email;
