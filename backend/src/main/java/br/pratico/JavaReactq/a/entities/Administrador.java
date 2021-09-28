b1a453e2f20ecdb5a5395d9d10752957e6ec9151
Loveneykens Philogene <loveneykens@gmail.com>
2021-09-27 22:28:19 -0300

    entity Administrador criada

---
+++ backend/src/main/java/br/pratico/JavaReactq/a/entities/Administrador.java
@@ -0,0 +1,67 @@
+package br.pratico.JavaReactq.a.entities;
+
+import java.io.Serializable;
+
+public class Administrador implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String sobreNome;
+	private String email;
+	private String telefone;
+	
+	public Administrador() {
+		
+	}
+	
+	public Administrador (Long id, String nome,String sobreNome,String email, String telefone) {
+		this.id=id;
+		this.nome=nome;
+		this.sobreNome=sobreNome;
+		this.email=email;
+		this.telefone=telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
